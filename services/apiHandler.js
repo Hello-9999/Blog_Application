3738f2952e84da32c930ef694006cbc349af9168
Gautam Magar <puskarmagar687@gmail.com>
2024-03-22 05:45:00 +0545

    feat: Implemented function to fetch individual blog details using SSG

--- services/apiHandler.js
+++ services/apiHandler.js
@@ -1,7 +1,18 @@
+// fetch blog data using SSG
 export const getBlogs = async () => {
   const data = await fetch(process.env.SERVER_URL, {
+    next: { revalidate: 60 },
+  });
+
+  return data.json();
+};
+
+export const getBlogsDetails = async (id) => {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
     cache: "force-cache",
   });
   return data.json();
 };
+
+
 
