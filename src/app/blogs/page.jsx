69382c52c8100d011bfa49570bf06c48076d64a8
Gautam Magar <puskarmagar687@gmail.com>
2024-03-22 12:29:20 +0545

    feat: Updated UI for blog list page

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,42 +1,58 @@
-import React from "react";
-import Link from "next/link";
+import { Link } from "@nextui-org/react";
 import { getBlogs } from "../../../services/apiHandler";
 import {
   Card,
   CardHeader,
   CardBody,
-  CardFooter,
   Divider,
+  Image,
 } from "@nextui-org/react";
-import { Pagination } from "@nextui-org/react";
 
 const Blogs = async () => {
   const blogsdata = await getBlogs();
 
   return (
-    <div>
-      <div className="title text-center mb-5 mt-5 font-bold  text-xl md:text-2xl">
+    <div className=" bg-slate-400">
+      <div className="title text-center mb-5 pt-7 font-bold  text-2xl md:text-3xl lg:text-4xl">
         {" "}
         Our Blogs
       </div>
-      <div className=" flex flex-wrap gap-5   w-10/12 m-auto md:w-5/6 justify-evenly">
+      <div
+        className=" gap-9 mt-6 pb-6 w-10/12 m-auto md:w-5/6 justify-evenly "
+        id="Blog_Card"
+      >
         {blogsdata &&
-          blogsdata.splice(0, 8).map((val) => {
-
+          blogsdata.splice(0, 5).map((val, index) => {
             return (
-              <Card className=" md:w-5/12">
+              <Card
+                className="  ,bg-neutral-100	hover:bg-slate-230 "
+                key={val.index}
+                style={{ border: "1px solid black" }}
+              >
                 {" "}
-                <CardHeader className="">
-                  <p className=" text-center font-semibold	 ">{val.title}</p>
-                </CardHeader>
-                <Divider />
-                <CardBody>
-                  {" "}
-                  <p>{val.body}</p>
+                <Link href={`/blogs/${val.id}`}>
+                  <CardHeader className="">
+                    <p className=" text-justify font-semibold p-3">
+                      {val.title}
+                    </p>{" "}
+                  </CardHeader>
+                </Link>
+                <Divider className="bg-red-300" />
+                <CardBody style={{ overflow: "hidden" }}>
+                  <div className="img m-auto">
+                    <Image src={`https://picsum.photos/id/${val.id}/550/300`} />
+                  </div>
+                  <p className="mt-5 mb-5">
+                    {val.body.substring(0, 100) + " ..."}
+                    <Link href={`/blogs/${val.id}`} color="primary">
+                      {" "}
+                      Read More
+                    </Link>
+                  </p>
                 </CardBody>
               </Card>
             );
-          }, <Pagination />)}
+          })}
       </div>
     </div>
   );
